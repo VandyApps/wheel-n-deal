6f3d076977aa63bec3c1d7239f4e4efd4f8a8aca
AliceCengal <cengallut@gmail.com>
2014-12-09 01:23:55 -0600

    defining server API

--- server/src/main/scala/Main.scala
+++ server/src/main/scala/Main.scala
@@ -37,7 +37,63 @@ object Main extends App {
 class GameController extends Controller {
 
   get("/test") { request =>
+    /* Returns a web page that tests the HTTP API of this server. */
     render.plain("Hello").toFuture
+  
   }
 
+  get("/game") { request =>
+    /* Returns an array of all game ids active. */
+  
+  }
+  
+  post("/game") { request =>
+    /* Creates a new game session and returns the session id. Session id
+     * is a randomly generated string of unspecified length. 
+     */
+  
+  }
+  
+  get("/game/:gameid/player") { request =>
+    /* Returns an array of all player ids active in this game session. */
+  
+  }
+  
+  post("/game/:gameid/player") { request =>
+    /* Creates a new player session and returns the player id. Player id
+     * is a randomly generated string of unspecified length. 
+     */
+    
+  }
+  
+  delete("/game/:gameid/player/:playerid") { request =>
+    /* Removes the player with the given id from this game session.
+     */
+    
+  }
+  
+  get("/game/:gameid/player/:playerid/events") { request =>
+    /* Returns game events.
+     * 
+     * Options:
+     *     mode ->
+     *         all    | Returns all events (default)
+     *         latest | Returns only the events since the last request
+     *         [0-9]+ | Returns the specified number of events
+     * 
+     */
+  
+  }
+  
+  post("/game/:gameid/player/:playerid/events") { request =>
+    /* Registers a game event. The event should be passed in in the body
+     * of the request. The event should be a valid JSON object.
+     * 
+     * Each event will be assigned a timestamp once it's received by the server.
+     * A UUID will be assigned, which identifies all event by its content and
+     * time of receipt.
+     */
+    
+  }
+  
 }
